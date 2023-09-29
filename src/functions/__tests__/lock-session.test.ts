5acb02d60a8a91648c908315ab16c77bea822084
arcogabbo <arcogabbo@gmail.com>
2023-09-29 15:31:52 +0200

    [#IOPID-857] lock-session handler

---
+++ src/functions/__tests__/lock-session.test.ts
@@ -0,0 +1,134 @@
+import { makeLockSessionHandler } from "../lock-session";
+import { httpHandlerInputMocks } from "../__mocks__/handlerMocks";
+import * as H from "@pagopa/handler-kit";
+import { aFiscalCode } from "../__mocks__/general";
+import * as E from "fp-ts/lib/Either";
+
+const aValidBody = { fiscal_code: aFiscalCode, unlock_code: "123456789" };
+
+const mockLockUserSession = jest
+  .fn()
+  .mockResolvedValue(E.right({ status: 201 }));
+const mockBackendInternalClient = {
+  authLock: mockLockUserSession
+} as any;
+
+describe("LockSession handler", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return 204 with a valid payload", async () => {
+    const mockReq: H.HttpRequest = {
+      ...H.request("https://api.test.it/"),
+      body: aValidBody
+    };
+    const result = await makeLockSessionHandler({
+      ...httpHandlerInputMocks,
+      input: mockReq,
+      backendInternalClient: mockBackendInternalClient
+    })();
+
+    expect(mockLockUserSession).toHaveBeenCalled();
+    expect(mockLockUserSession).toHaveBeenCalledWith({
+      fiscalcode: aValidBody.fiscal_code,
+      unlockcode: aValidBody.unlock_code
+    });
+    expect(E.isRight(result)).toEqual(true);
+    expect(result).toMatchObject({ right: { statusCode: 204, body: null } });
+  });
+
+  it("should return 409 when the user is already locked", async () => {
+    mockLockUserSession.mockResolvedValueOnce(E.right({ status: 409 }));
+    const mockReq: H.HttpRequest = {
+      ...H.request("https://api.test.it/"),
+      body: aValidBody
+    };
+    const result = await makeLockSessionHandler({
+      ...httpHandlerInputMocks,
+      input: mockReq,
+      backendInternalClient: mockBackendInternalClient
+    })();
+
+    expect(mockLockUserSession).toHaveBeenCalled();
+    expect(E.isRight(result)).toEqual(false);
+    expect(result).toMatchObject({
+      left: { status: 409, title: "Conflict" }
+    });
+  });
+
+  it.each`
+    responseCode
+    ${400}
+    ${401}
+    ${500}
+  `(
+    "should return an internal error with a $responseCode from the downstream component",
+    async ({ responseCode }) => {
+      mockLockUserSession.mockResolvedValueOnce(
+        E.right({ status: responseCode })
+      );
+      const mockReq: H.HttpRequest = {
+        ...H.request("https://api.test.it/"),
+        body: aValidBody
+      };
+      const result = await makeLockSessionHandler({
+        ...httpHandlerInputMocks,
+        input: mockReq,
+        backendInternalClient: mockBackendInternalClient
+      })();
+
+      expect(mockLockUserSession).toHaveBeenCalled();
+      expect(E.isRight(result)).toEqual(false);
+      expect(result).toMatchObject({
+        left: { status: 500, title: "Internal Server Error" }
+      });
+    }
+  );
+
+  it("should return an internal error when the downstream component is unreachable via network", async () => {
+    mockLockUserSession.mockRejectedValueOnce({});
+    const mockReq: H.HttpRequest = {
+      ...H.request("https://api.test.it/"),
+      body: aValidBody
+    };
+    const result = await makeLockSessionHandler({
+      ...httpHandlerInputMocks,
+      input: mockReq,
+      backendInternalClient: mockBackendInternalClient
+    })();
+
+    expect(mockLockUserSession).toHaveBeenCalled();
+    expect(mockLockUserSession).toHaveBeenCalledWith({
+      fiscalcode: aValidBody.fiscal_code,
+      unlockcode: aValidBody.unlock_code
+    });
+    expect(E.isRight(result)).toEqual(false);
+    expect(result).toMatchObject({
+      left: { status: 500, title: "Internal Server Error" }
+    });
+  });
+
+  it("should return an internal error when the downstream component gives an unexpected response", async () => {
+    mockLockUserSession.mockResolvedValueOnce(E.left({}));
+    const mockReq: H.HttpRequest = {
+      ...H.request("https://api.test.it/"),
+      body: aValidBody
+    };
+    const result = await makeLockSessionHandler({
+      ...httpHandlerInputMocks,
+      input: mockReq,
+      backendInternalClient: mockBackendInternalClient
+    })();
+
+    expect(mockLockUserSession).toHaveBeenCalled();
+    expect(mockLockUserSession).toHaveBeenCalledWith({
+      fiscalcode: aValidBody.fiscal_code,
+      unlockcode: aValidBody.unlock_code
+    });
+    expect(E.isRight(result)).toEqual(false);
+    expect(result).toMatchObject({
+      left: { status: 500, title: "Internal Server Error" }
+    });
+  });
+});
