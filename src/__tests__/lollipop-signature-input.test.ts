05e0cb60def4ef463bbc4896fd104f3ca234783e
Daniele Manni <dan.manni@gmail.com>
2024-03-07 12:04:37 +0100

    [#IOPID-1642] Adescription over signature input constants

--- src/__tests__/lollipop-signature-input.test.ts
+++ src/__tests__/lollipop-signature-input.test.ts
@@ -2,7 +2,9 @@ import { PatternString } from "@pagopa/ts-commons/lib/strings";
 import { LollipopSignatureInput } from "../generated/definitions/internal/LollipopSignatureInput";
 import * as E from "fp-ts/Either";
 
+// A LolliPoP valid SignatureInput with multiple signature
 const aValidMultiSignatureInput = `sig1=("@method" "@authority" "@path" "content-digest" "content-type" "content-length" "x-pagopa-original-url" "x-pagopa-original-method");created=1618884475;keyid="test-key-ecc-p256", sig2=("@method" "@authority" "@path" "content-digest" "content-type" "content-length" "forwarded");created=1618884480;keyid="test-key-rsa";alg="rsa-v1_5-sha256";expires=1618884540`;
+// A LolliPoP valid SignatureInput with single signature
 const aValidSingleSignatureInput = `sig1=("@method" "@authority" "@path" "content-digest" "content-type" "content-length" "x-pagopa-original-url" "x-pagopa-original-method");created=1618884475;keyid="test-key-ecc-p256"`;
 
 // The following string is an example of input value that can execute a ReDOS
